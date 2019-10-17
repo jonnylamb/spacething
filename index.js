916033a9115b4d671dde4d2a8ce7c302245eba65
Jonny Lamb <jonnylamb@jonnylamb.com>
2019-10-17 15:22:16 +0100

    check the current month from the html before progressing

--- index.js
+++ index.js
@@ -6,30 +6,43 @@ const month = new Date().getMonth();
 async function run() {
     // ouch...
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
-    const page = await browser.newPage();
 
-    // open page
-    await page.goto(URL);
-    await page.waitFor(5000);
+    try {
+        const page = await browser.newPage();
 
-    // click 'Select Date/Time'
-    await page.click('#page > div.container.main > div:nth-child(4) > div.viewItems.ng-scope > div.module.ng-scope > div.sub-category.b-b-all.ng-scope.open > div.list-container.m-b-all > div > div:nth-child(8) > div > div.row.ng-scope > div > div.row.m-t-all > div.c.c-100-c > span > button')
-    await page.waitFor(3000);
+        // open page
+        await page.goto(URL);
+        await page.waitFor(5000);
 
-    // this will break after january
-    for (var i = month; (i % 12) > 0; i++) {
-        // click next month
-        await page.click('#page > div:nth-child(6) > div.multi-time-selector-modal.calendar-modal.event-time.ng-scope > div.modal.info-modal.w-auto-c.ng-scope > div > div:nth-child(3) > div > div > div > div > div.calendar-header > div.row.month-name > div:nth-child(4) > span')
+        // click 'Select Date/Time'
+        await page.click('#page > div.container.main > div:nth-child(4) > div.viewItems.ng-scope > div.module.ng-scope > div.sub-category.b-b-all.ng-scope.open > div.list-container.m-b-all > div > div:nth-child(8) > div > div.row.ng-scope > div > div.row.m-t-all > div.c.c-100-c > span > button')
         await page.waitFor(3000);
-    }
 
-    // find the right day
-    const dayIsDisabled = await page.evaluate(() => document.querySelector('div[aria-label="1/13/2020"]').getAttribute('aria-disabled'))
+        async function isJanuary() {
+            const selected = await page.evaluate(() => {
+                const s = document.querySelector('#page > div:nth-child(6) > div.multi-time-selector-modal.calendar-modal.event-time.ng-scope > div.modal.info-modal.w-auto-c.ng-scope > div > div:nth-child(3) > div > div > div > div > div.calendar-header > div.row.month-name > div.c.c-40-all.month > span > select')
+                return s.options[s.selectedIndex].text;
+            })
+            return selected === 'January';
+        };
 
-    // work out
-    const available = dayIsDisabled !== 'true';
-    if (available) {
-        console.log('booking for day (13th Jan) could be available');
+        while (!await isJanuary()) {
+            // click next month
+            await page.click('#page > div:nth-child(6) > div.multi-time-selector-modal.calendar-modal.event-time.ng-scope > div.modal.info-modal.w-auto-c.ng-scope > div > div:nth-child(3) > div > div > div > div > div.calendar-header > div.row.month-name > div:nth-child(4) > span')
+            await page.waitFor(3000);
+        }
+
+        // find the right day
+        const dayIsDisabled = await page.evaluate(() => document.querySelector('div[aria-label="1/13/2020"]').getAttribute('aria-disabled'))
+
+        // work out
+        const available = dayIsDisabled !== 'true';
+        if (available) {
+            console.log('booking for day (13th Jan) could be available');
+        }
+
+    } catch (error) {
+        console.error(error);
     }
 
     browser.close();
